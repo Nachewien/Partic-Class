29f42e4ad2ab81cbae9360895c944d81c0529260
Nachewien <ignaciogaraffini@gmail.com>
2025-06-05 21:18:12 -0300

    creacion de frontend con react

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@
 const express = require('express');         // Import express to create the server
 const bodyParser = require('body-parser');  // Import body-parser to parse incoming request bodies
 const cors = require('cors');               // Import routes
-const routes = require('./routes');         // Import routes from the routes directory
+//const routes = require('/routes');         // Import routes from the routes directory
 
 
 // Initialize the app
@@ -23,19 +23,24 @@ app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
 
-// Endpoint para obtener todos los usuarios
-app.get('/usuarios', (req, res) => {
-  const sql = 'SELECT * FROM usuarios';
 
-  db.query(sql, (err, results) => {
-    if (err) {
-      console.error('Error al ejecutar la consulta:', err);
-      res.status(500).json({ error: 'Error en la base de datos' });
-    } else {
-      res.json(results);
-    }
-  });
-});
+// Endpoint para obtener todos los usuarios // posteriormente se moverá a un archivo de rutas
+// const mysql = require('mysql'); // Import MySQL module  
+
+// app.get('/usuarios', (req, res) => {
+//   const sql = 'SELECT * FROM usuario';
+
+//   db.query(sql, (err, results) => {
+//     if (err) {
+//       console.error('Error al ejecutar la consulta:', err);
+//       res.status(500).json({ error: 'Error en la base de datos' });
+//     } else {
+//       res.json(results);
+//     }
+//   });
+// });
+
+
 
 // HOST     192.168.100.103
 // PORT     3306 
